1bdcc253eafb867c3854f3f6e007fb12b3c67520
rush0714 <pukanGi9>
2016-10-11 14:00:14 +0300

    comment

--- src/main/java/test/com/Api/Cache.java
+++ src/main/java/test/com/Api/Cache.java
@@ -1,8 +1,9 @@
 package test.com.Api;
 
 import java.io.IOException;
+import java.io.Serializable;
 
-public interface Cache<K, V> {
+public interface Cache<K, V extends Serializable> {
     void put(K key, V value) throws IOException;
     V get(K key) throws IOException, ClassNotFoundException;
     boolean contains(K key);
